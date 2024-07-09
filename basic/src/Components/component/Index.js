9533a58384c6910a0fe92b1e3a45e1f81714681f
Akshay Kanawade <akshay.kanawade@app-scoop.com>
2024-07-09 20:12:48 +0530

    Created component

---
+++ basic/src/Components/component/Index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FunctionalComp from './FunctionalComp';
+import ClassComp from './ClassComp';
+
+function Component() {
+  return (
+    <div>
+        <FunctionalComp />
+        <ClassComp/>
+      
+    </div>
+  );
+}
+
+export default Component;
